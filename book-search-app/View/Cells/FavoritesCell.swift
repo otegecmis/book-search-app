171414170e1bdc07f30f7113acd53d833c36fc15
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-17 18:09:49 +0300

    feat: create `favorites` functionality

---
+++ book-search-app/View/Cells/FavoritesCell.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class FavoritesCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static let reuseID = "FavoritesCell"
+    let bookCoverImageView = BookCoverImageView(frame: .zero)
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    func set(book: Book) {
+        bookCoverImageView.downloadImage(from: book.image)
+    }
+    
+    private func configure() {
+        addSubview(bookCoverImageView)
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            bookCoverImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            bookCoverImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            bookCoverImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            bookCoverImageView.heightAnchor.constraint(equalTo: bookCoverImageView.widthAnchor, multiplier: 1.5),
+        ])
+    }
+}
