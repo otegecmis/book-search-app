d55e42e16ac3ed2b3983886d39690e060c826c0d
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-25 04:06:40 +0300

    refactor: improve code `readability` for `extensions` and `controller` (#19)

--- book-search-app/Controller/MainTabController.swift
+++ book-search-app/Controller/MainTabController.swift
@@ -12,28 +12,14 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         view.backgroundColor = .systemBackground
         
-        let search = templateNavigationController(title: "Search", image: UIImage(systemName: "magnifyingglass"), rootViewController: SearchController())
-        let favorites = templateNavigationController(title: "Favorites", image: UIImage(systemName: "heart"), rootViewController: FavoritesController())
+        let search = navigationTabController(title: "Search", image: UIImage(systemName: "magnifyingglass"), rootViewController: SearchController())
+        let favorites = navigationTabController(title: "Favorites", image: UIImage(systemName: "heart"), rootViewController: FavoritesController())
         
         viewControllers = [search, favorites]
-    }
-    
-    func templateNavigationController(title: String, image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
-        let nav = UINavigationController(rootViewController: rootViewController)
-        let appearance = UITabBar.appearance()
-        
-        nav.title = title
-        nav.view.backgroundColor = .systemBackground
-        nav.tabBarItem.image = image
-        nav.tabBarItem.selectedImage?.withTintColor(UIColor.red)
-        appearance.tintColor = .systemRed
-        
-        return nav
     }
 }
 
 @available(iOS 17.0, *)
 #Preview {
-    let mainTabController = MainTabController()
-    return mainTabController
+    return MainTabController()
 }
