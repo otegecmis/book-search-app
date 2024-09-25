9500f6332d9513214ee1a0650bd83fa2f8416ae1
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-25 04:05:25 +0300

    refactor: improve code `readability` for `extensions` and `controller`

--- book-search-app/Utils/Extensions.swift
+++ book-search-app/Utils/Extensions.swift
@@ -2,6 +2,7 @@ import UIKit
 
 fileprivate var containerView: UIView!
 
+// MARK: - UIViewController
 extension UIViewController {
     
     // MARK: - Helpers
@@ -13,5 +14,23 @@ extension UIViewController {
             alertVC.completion = completion
             self.present(alertVC, animated: true)
         }
+    }
+}
+
+// MARK: - UITabBarController
+extension UITabBarController {
+    
+    // MARK: - Helpers
+    func navigationTabController(title: String, image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        let appearance = UITabBar.appearance()
+        
+        nav.title = title
+        nav.view.backgroundColor = .systemBackground
+        nav.tabBarItem.image = image
+        nav.tabBarItem.selectedImage?.withTintColor(UIColor.systemBrown)
+        appearance.tintColor = .systemBrown
+        
+        return nav
     }
 }
